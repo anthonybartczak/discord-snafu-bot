626698da330c3a24f9912500d94b7f63ae6464f5
anthonybartczak <antoni.bartczak.w@gmail.com>
2021-04-26 13:12:03 +0200

    Role specific messages

--- bot.js
+++ bot.js
@@ -1,25 +1,28 @@
 const Discord = require('discord.js');
+const {NodeSSH} = require('node-ssh')
 
 const client = new Discord.Client();
-
+const ssh = new NodeSSH()
  
 
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
 
  
 
 client.on('message', message => {
-
     if (message.content === 'ping') {
-
        message.reply('pong');
+       }    
+});
 
-       }
-
+client.on('message', message => {
+    if (message.content === 'ping') {
+        if(message.member.roles.find(r => r.name === "Zarząd")){
+            message.reply('pong');
+        }
+    }    
 });
 
  
